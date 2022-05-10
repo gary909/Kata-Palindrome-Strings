960d86670f8f323d191af161b5eac2be603f8b3e
gary <gary.white@lodgit.com>
2022-05-10 13:19:35 +0200

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function isPalindrome(line) {
+    return undefined;
+}
+
+console.log(isPalindrome("anna")); // true
+console.log(isPalindrome("walter")); // false
+console.log(isPalindrome("12321")); // true
+console.log(isPalindrome("123456")); // false
+console.log(isPalindrome(".")); // true
+console.log(isPalindrome(".!!.")); // true
